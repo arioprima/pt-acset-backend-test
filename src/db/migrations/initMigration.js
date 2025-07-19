6c201127e4ae7c16f9ab5587f256c3d144c3b515
Ario Prima <arioprima2@gmail.com>
2025-07-19 21:36:39 +0700

    feat: add counter and machin seeder

--- src/db/migrations/initMigration.js
+++ src/db/migrations/initMigration.js
@@ -1,6 +1,8 @@
 const hashPassword = require("../../helpers/hashPassword");
 const Branch = require("../../models/branch.model");
 const User = require("../../models/user.model");
+const Counter = require("../../models/counter.model");
+const Machine = require("../../models/machine.model");
 
 async function initMigration() {
     const count = await Branch.countDocuments();
@@ -21,6 +23,26 @@ async function initMigration() {
         password: await hashPassword("admin123"),
         role: "admin",
         branch_id: branch._id,
+        created_at: new Date(),
+    });
+
+    await Counter.create({
+        branch_id: branch._id,
+        name: "Counter 1",
+        created_at: new Date(),
+    });
+
+    await Machine.create({
+        branch_id: branch._id,
+        uuid: "machine-001",
+        type: "antrian",
+        created_at: new Date(),
+    });
+
+    await Machine.create({
+        branch_id: branch._id,
+        uuid: "machine-002",
+        type: "display",
         created_at: new Date(),
     });
 }
